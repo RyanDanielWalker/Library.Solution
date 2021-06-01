cdc23504b72ea2d1ad643c2c83d4b266bd6c9341
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-01 14:26:44 -0700

    Update book controller with details, create, edit
    Co-authored-by:  Jesse White <jesse.white6@gmail.com>
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -29,5 +29,65 @@ namespace Library.Models
       var userBooks = _db.Books.Where(entry => entry.User.Id == currentUser.Id).ToList();
       return View(userBooks);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "FirstName");
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Book book, int authorId)
+    {
+      var userId = this._userManager.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      // bool authorExists = _db.Authors
+      //   .Any(entry => entry.FirstName == author.FirstName && entry.LastName == author.LastName);
+      // if (!authorExists)
+      // {
+      //   _db.Authors.Add(author);
+      //   Author selectedAuthor = author;
+      // }
+      // else
+      // {
+      //   Author selectedAuthor = _db.Authors
+      //     .FirstOrDefault(entry => entry.FirstName == author.FirstName && entry.LastName == author.LastName);
+      // }
+      _db.Books.Add(book);
+      _db.SaveChanges();
+      if (authorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() { AuthorId = authorId, BookId = book.BookId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+    [AllowAnonymous]
+    public ActionResult Details(int id)
+    {
+      var thisBook = _db.Books
+          .Includes(book => book.JoinEntities)
+          .ThenIncludes(join => join.Author)
+          .FirstOrDefault(book => book.BookId == id);
+      return (thisBook);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "FirstName");
+      return View(thisBook);
+    }
+    [HttpPost]
+    public ActionResult Edit(Book book, int AuthorId)
+    {
+      if (AuthorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() { AuthorId = AuthorId, BookId = book.BookId });
+      }
+      _db.Entry(book).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
+
